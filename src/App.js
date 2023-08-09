5d4575c7541bbea53969b0b43ab431ab843a3687
Sunilmehra1 <141426110+Sunilmehra1@users.noreply.github.com>
2023-08-09 23:57:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react'
-import {BrowserRouter , Route , Routes, useNavigate} from "react-router-dom"
+// import React, { useEffect } from 'react'
+import {BrowserRouter , Route , Routes,} from "react-router-dom"
 // import Home from './component/Home'
-import Login from './componenet/Login'
-import Signup from './componenet/Signup'
-import Dashboard from './componenet/Dashboard'
-import Home from './componenet/Home'
+import Login from './component/Login'
+import Signup from './component/Signup'
+import Dashboard from './component/Dashboard'
+import Home from './component/Home'
 // import Login from './component/login'
 
 const App = () => {
@@ -15,8 +15,8 @@ const App = () => {
     <div>
 <BrowserRouter>
 <Routes>
-  <Route  path='/' element = {<Home/>}/>
-  <Route path='/sign' element={<Login/>}/>
+<Route  path='/'  element ={<Home/>}/>
+  <Route  path='/sign' element = {<Login/>}/>
   <Route path='/lo' element={<Signup/>}/>
   <Route path='/dashboard' element={<Dashboard />}/>
 </Routes>
---
+++ src/component/Dashboard.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import { json, useNavigate } from "react-router-dom";
+import QRCode from "react-qr-code";
+
+export default function Dashboard() {
+  
+  const [input, setinput] = useState({
+    task: ""
+  });
+
+  const [data , setdata]  = useState([])
+  let task = localStorage.getItem("Task");
+
+  useEffect(() => {
+    if (task) {
+      JSON.parse(task);
+      setdata(JSON.parse(task))
+      
+    }
+    
+  }, []);
+
+  let user = localStorage.getItem("token");
+
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (user === null) {
+      navigate("/");
+    }
+  }, [user]);
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    navigate("/");
+  };
+
+  const handle = (e) => {
+    setinput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+
+    if (localStorage.getItem("Task")) {
+      let task = JSON.parse(localStorage.getItem("Task"));
+      localStorage.setItem("Task", JSON.stringify([{...input , barcode : input.task} , ...task]));
+      window.location.reload(false)
+    } else {
+      localStorage.setItem("Task", JSON.stringify([{...input , barcode : input.task}]));
+      window.location.reload(false)
+    }
+  };
+
+console.log(data)
+
+  return (
+    <div className="bg-blue-500 h-[100vh] flex justify-between align-baseline p-2">
+      <form className="flex flex-col bg-white w-[30%] h-[60vh] space-y-5 items-center  p-3 rounded-sm  overflow-y-auto ">
+        <input
+          type="name"
+          className="w-[100%] border border-black bg-white py-2 px-1 rounded"
+          name="task"
+          value={input.task}
+          onChange={handle}
+          required
+        />
+
+        <button
+          className="w-[100%] px-2 justify-center bg-green-500 rounded-sm text-white py-3 flex items-center "
+          onClick={handlesubmit}
+        >
+          Task
+        </button>
+        {
+data?.map((datas)=>{
+  
+  return  <>
+  <h1>{datas?.task}</h1>
+  <div style={{ background: 'white', padding: '16px' }}>
+  <QRCode value={datas?.task}  className="h-[40px]"/>
+</div>
+
+  </>
+})
+
+        }
+      </form>
+
+      <button onClick={() => logout()} className="bg-green-500 rounded-lg text-white p-2">Logout</button>
+    </div>
+  );
+}
