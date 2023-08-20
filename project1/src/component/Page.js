cee2b8341faa5c8e7119d0d70a2c4d531fac244f
Sunilmehra1 <141426110+Sunilmehra1@users.noreply.github.com>
2023-08-20 14:46:06 +0530

    changes

---
+++ project1/src/component/Page.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react'
+
+const Page = () => {
+
+  return (
+    <div className='flex justify-center items-center text-lg font-extrabold capitalize bg-green-500 h-[100vh]'>signup is completed</div>
+  )
+}
+
+export default Page
